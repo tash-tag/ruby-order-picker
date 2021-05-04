fdc7c429e54a93f5f45eabdb0adc9e0d575ff3d0
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-05-04 12:10:03 +1000

    implements random loop

--- index.rb
+++ index.rb
@@ -1,6 +1,7 @@
-group = []
+group = ["alice", "bob", "charlie", "dylan"]
 
 while true 
+    puts "There are #{group.length} members in the group"
     puts "Press 1) to add a member to the group"
     puts "Press 2) display a random group"
     puts "Press 3) to quit"
@@ -14,7 +15,21 @@ while true
         group.push(name)
     when 2 
         puts "Random group output:"
-        #TODO - Output random group
+        random_order_loop_running = true
+        while random_order_loop_running
+            copied_group = group.clone 
+            while copied_group.length > 0
+                random_index = rand(copied_group.length - 1)
+                puts copied_group[random_index].capitalize
+                copied_group.delete_at(random_index)
+            end 
+            puts "press 1 to repeat"
+            puts "press 2 to quit"
+            choice = gets.chomp.to_i 
+            if choice == 2 
+                exit
+            end
+        end 
     when 3
         #TODO - Check they want to quit
         break
